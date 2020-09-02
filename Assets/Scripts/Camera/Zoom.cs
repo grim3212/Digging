fdf9bc20d077cec521343d54dea2fc36f055fa92
Dakota Pierone <dakotapierone@yahoo.com>
2020-09-02 06:50:52 -0700

    Add in the ability to zoom, needs work to look better with camera follow

---
+++ Assets/Scripts/Camera/Zoom.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+	[SerializeField]
+	private Camera ppwzCamera;
+	private PixelPerfect ppwz;
+
+	void Start () {
+		ppwz = ppwzCamera.GetComponent<PixelPerfect> ();
+	}
+
+	void Update () {
+		if (Input.mouseScrollDelta.y != 0) {
+			if (Input.mouseScrollDelta.y > 0) {
+				ppwz.ZoomIn ();
+			}
+			else {
+				ppwz.ZoomOut ();
+			}
+		}
+	}
+}
--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,30 +4,33 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 	public Transform player;
-    public float smoothSpeed = 0.5f;
+	public float smoothSpeed = 1f;
 
 	private Vector2 worldMin;
-    private Vector2 worldMax;
+	private Vector2 worldMax;
 	private float camY, camX;
 	private float camOrthsize;
 	private float cameraRatio;
 	private Camera mainCam;
-    private Vector3 smoothPos;
+	private Vector3 smoothPos;
 
 	void Start () {
-        Bounds localBounds = World.Instance.Colliders.localBounds;
-        worldMin = World.Instance.Colliders.transform.TransformPoint(localBounds.min);
-        worldMax = World.Instance.Colliders.transform.TransformPoint(localBounds.max);
-		
+		Bounds localBounds = World.Instance.Colliders.localBounds;
+		worldMin = World.Instance.Colliders.transform.TransformPoint (localBounds.min);
+		worldMax = World.Instance.Colliders.transform.TransformPoint (localBounds.max);
+
 		mainCam = GetComponent<Camera> ();
-		camOrthsize = mainCam.orthographicSize;
-		cameraRatio = mainCam.aspect * camOrthsize;
+		
 	}
 
 	void FixedUpdate () {
+		// These values could change with the zoom
+		camOrthsize = mainCam.orthographicSize;
+		cameraRatio = mainCam.aspect * camOrthsize;
+
 		camY = Mathf.Clamp (this.player.position.y, worldMin.y + camOrthsize, worldMax.y - camOrthsize);
 		camX = Mathf.Clamp (this.player.position.x, worldMin.x + cameraRatio, worldMax.x - cameraRatio);
-        smoothPos = Vector3.Lerp(this.transform.position, new Vector3(camX, camY, this.transform.position.z), smoothSpeed);
-		this.transform.position = smoothPos;
+		smoothPos = Vector3.Lerp (this.transform.position, new Vector3 (camX, camY, this.transform.position.z), smoothSpeed);
+		this.transform.position = new Vector3 (camX, camY, this.transform.position.z);
 	}
 }
